85a86bead3a570dc5d78e84367a9798ddb86b5f7
daniela <danniela.alejandra@gmail.com>
2025-03-17 21:52:05 -0300

    agregando archivos en rama

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,22 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/NavBar';
+import Modal from 'react-bootstrap/Modal';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const NavBar = () => {
     const total = 25000;
     const token = false;
 
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
   return (
+    <>
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="#home">Pizzeria Mamma Mia!</Navbar.Brand>
@@ -21,8 +31,8 @@ const NavBar = () => {
             </>
             ) : (
             <>
-            <Nav.Link eventKey='login' href="#Login" to='/Login' >🔐Login</Nav.Link>
-            <Nav.Link eventKey='register' href="#Register" to='/Register'>🔐Register</Nav.Link>
+            <Nav.Link eventKey='login' href="#Login" onClick={handleShow}>🔐Login</Nav.Link>
+            <Nav.Link eventKey='register' href="#Register" >🔐Register</Nav.Link>
             </>
             )}
             </Nav>
@@ -32,6 +42,41 @@ const NavBar = () => {
         </Navbar.Collapse>
       </Container>
     </Navbar>
+
+<Modal show={show} onHide={handleClose}>
+<Modal.Header closeButton>
+  <Modal.Title>Login</Modal.Title>
+</Modal.Header>
+<Modal.Body>
+  <Form>
+    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+      <Form.Label>Email address</Form.Label>
+      <Form.Control
+        type="email"
+        placeholder="name@example.com"
+        autoFocus
+      />
+    </Form.Group>
+    <Form.Group
+      className="mb-3"
+      controlId="exampleForm.ControlTextarea1"
+    >
+      <Form.Label>Example textarea</Form.Label>
+      <Form.Control as="textarea" rows={3} />
+    </Form.Group>
+  </Form>
+</Modal.Body>
+<Modal.Footer>
+  <Button variant="secondary" onClick={handleClose}>
+    Close
+  </Button>
+  <Button variant="primary" onClick={handleClose}>
+    Save Changes
+  </Button>
+</Modal.Footer>
+</Modal>
+
+</>
   );
 };
 

84cc9e3074624791cb0cd771e0212e5f7b04a8b4
daniela <danniela.alejandra@gmail.com>
2025-03-20 14:15:52 -0300

    Agregue componente login y login modal para reestructurar componente Navbar

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,125 +3,63 @@
 import React from 'react'
 import Modal from 'react-bootstrap/Modal';
 import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import LoginModal from './LoginModal';
 
-const Login = () => {
-
-
-        const [show, setShow] = useState(false);
+const Login = ({ show,  handleClose }) => {
         const [email, setEmail] = useState ('')
-            const [contraseña, setContraseña] = useState ('')
-            const [confirmarContraseña, setConfirmarContraseña] = useState ('')
-            const [error, setError] = useState (false)
-            const [smShow, setSmShow] = useState(false);
-
-            const handleShow = () => setShow(true);
-        
-        
+        const [contraseña, setContraseña] = useState ('')
+        const [confirmarContraseña, setConfirmarContraseña] = useState ('')
+        const [error, setError] = useState (false)
+        const [smShow, setSmShow] = useState(false);
+        // const [show, setShow] = useState(false);
+        // const handleShow = () => setShow(true);
+        // const handleClose = () => {
+        //   setShow(false);
+        // Validacion de formulario
             const handleSubmit = (e) => {
                 e.preventDefault();
                 if(email === ''){
-                    setError(true)
+                    setError(true);
                     return
                 }
         
                 if(contraseña === '' || contraseña.length < 6){
-                    setError(true)
+                    setError(true);
                     return
                 }
         
                 if(confirmarContraseña === '' || (confirmarContraseña !== contraseña)){
-                    setError(true)
+                    setError(true);
                     return
                 }
-      
-                console.log(confirmarContraseña)
-                console.log(contraseña)
-             
-                
-      
-                setError(false)
-                setEmail('')
-                setContraseña('')
-                setConfirmarContraseña('')
-                setShow(false)
-                setSmShow(true)
-                console.log('Formulario enviado')
-            
-            }
-         
-            const handleChangeEmail = (e) => {
-                setEmail(e.target.value)
-            }
-        
-            const handleChangeContraseña = (e) => {
-                setContraseña(e.target.value)
-            }
-        
-            const handleChangeConfirmarContraseña = (e) => {
-                setConfirmarContraseña(e.target.value)
-            }
-    
-        const handleClose = () => {
-          setShow(false);
-        };
-       
-    
 
+                setError(false);
+                setEmail('');
+                setContraseña('');
+                setConfirmarContraseña('');
+                handleClose(); // Cierra el modal al enviar
+                setSmShow(true);
+                // setShow(false)
+                console.log('Formulario enviado');
+            };
+       
   return (
     <div>
-      <Modal show={show} onHide={handleClose}>
-<form action="submit" onSubmit={handleSubmit}>
-<Modal.Header closeButton>
-  <Modal.Title>Login</Modal.Title>
-</Modal.Header>
-<Modal.Body>
-        <label htmlFor="login" className='d-flex my-3 display-3'><strong>Login 🔐</strong></label>
-       
-        {error ? <p className='text-danger'>Todos los campos son obligarotios (no pueden estar vacíos)</p> : null }
+    {/* Modal Login */}
+    <LoginModal
+    show={show}
+    handleClose={handleClose}
+    handleSubmit={handleSubmit}
+    email={email}
+    contraseña={contraseña}
+    confirmarContraseña={confirmarContraseña}
+    handleChangeEmail={(e) => {setEmail(e.target.value)}}
+    handleChangeContraseña={(e) => {setContraseña(e.target.value)}}
+    handleChangeConfirmarContraseña={(e) => {setConfirmarContraseña(e.target.value)}}
+    error={error}
+    />
 
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email</Form.Label>
-        <Form.Control type="email" 
-        placeholder="nombre.apellido@ejemplo.com"
-        value={email}
-        onChange={(e) => handleChangeEmail (e)} />
-        <Form.Text className="text-muted">
-          Nunca compartiremos su email con nadie.
-        </Form.Text>
-      </Form.Group>
-    
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Contraseña</Form.Label>
-        <Form.Control type="password" 
-        placeholder="Ingrese su contraseña"
-        value={contraseña}
-        onChange={(e) => handleChangeContraseña (e)} />
-      </Form.Group>
-      {contraseña && contraseña.length < 6 ? <p className='text-danger'>La contraseña debe tener al menos 6 caracteres</p> : null }
-   
-       <Form.Group className="mb-3" controlId="BasicPassword">
-        <Form.Label>Confirme su contraseña</Form.Label>
-        <Form.Control type="password" 
-        placeholder="Ingrese su contraseña"
-        value={confirmarContraseña}
-        onChange={(e) => handleChangeConfirmarContraseña (e)} />
-      </Form.Group>
-      {contraseña !== confirmarContraseña ? <p className='text-danger'>Las contraseñas no coinciden, intente nuevamente</p> : null  }
-</Modal.Body>
-<Modal.Footer>
-  <Button variant="secondary" onClick={handleClose}>
-    Close
-  </Button>
-  <Button variant="primary" type='submit'>
-    Enviar
-  </Button>
-</Modal.Footer>
-      </form>
-</Modal>
-
-<div>
+    {/* Modal Bienvenida */}
       <Modal
         size="sm"
         show={smShow}
@@ -136,8 +74,7 @@ const Login = () => {
         <Modal.Body>Pizzeria Mamma Mia 🍕</Modal.Body>
       </Modal>
 </div>
-    </div>
-  )
-}
+  );
+};
 
 export default Login

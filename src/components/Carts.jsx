ff3d2d088ce1a4d8fcadd4748e7e35e3b408c835
daniela <danniela.alejandra@gmail.com>
2025-03-25 00:23:33 -0300

    Carrito de compras con funcionalidad listo

--- src/components/Carts.jsx
+++ src/components/Carts.jsx
@@ -11,15 +11,54 @@ import CardBody from 'react-bootstrap/esm/CardBody';
 
 const Carts = () => {
     const [cart, setCart] = useState(pizzaCart);
+    const [total, setTotal] = useState(0)
+    let agregarPizza = (id, precio) => {
+        setCart (cart.map((pizza) => {
+            if (pizza.id === id) {
+                return {...pizza, 
+                    cantidadPizza: (pizza.cantidadPizza ?? 0) + 1,
+                    total: precio * ((pizza.cantidadPizza ?? 0) + 1)}
+            }
+            sumarTotal()
+            return pizza;
+        }))
+    }
+
+
+    let eliminarPizza = (id, precio) => {
+        setCart (cart.map((pizza) => {
+            if (pizza.id === id) {
+                return {...pizza, 
+                    cantidadPizza: (pizza.cantidadPizza ?? 0) - 1,
+                    total: precio * ((pizza.cantidadPizza ?? 0) - 1)}
+            }
+            sumarTotal()
+            return pizza;
+        }));
+    }
+
+    const sumarTotal = () => {
+        return cart.reduce((acc, el) => {
+            return acc + el.total;
+        }, 0)
+        
+        console.log(total)
+    }
+
+    const handleChange = (e) => {
+        e.preventDefault
+        setCart(e.target.value)
+    }
 
     return (
     <>
       <Container>
-       
+
             <h2>Detalles del pedido</h2>
             {cart.map((carta) => (
             <Card key={carta.id}>
-            <Row xs={1} md={3}>
+                <Container>
+            <Row>
                 <Col>
                 <Card.Img
                 src={carta.img}
@@ -38,15 +77,32 @@ const Carts = () => {
                 <strong>$ {carta.price.toLocaleString()}</strong></Card.Text>
                 </Col>
 
-                <Col>
-                <button className=''>+</button>
+                <Col className='my-5'>
+                <Row>
+                <div className='d-flex'>
+                <Button variant="primary" size="lg" onClick={() => agregarPizza(carta.id, carta.price)}>+</Button>
+                <input className='text-center' type="number" value={carta.cantidadPizza??0} onChange={handleChange} />
+                <Button variant="danger" size="lg" onClick={() => eliminarPizza(carta.id, carta.price)}>-</Button>
+                </div>
+
+                </Row>
+
                 </Col>
+               
+                <Col>
+                <span> Subtotal $ {carta.total ?? 0} </span>
+
+                </Col>
+
             </Row>
-             
+
+            <Row>
+            </Row>
+            </Container>
             </Card>
             ))}
-    
-       
+
+            <h3 className='my-3 text-center'>Total${cart.reduce((acc, el) => acc + (el.total || 0), 0)}</h3>
       </Container>
     </>
   )
